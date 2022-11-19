bbdd5e1b0d73d6663cb687043e66dc42212865a5
Vinicius Boscoa <virb30@gmail.com>
2022-11-19 16:34:40 -0300

    feature: add create user route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,6 +26,9 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
@@ -33,5 +36,8 @@ func main() {
 	r.Get("/products", productHandler.GetProducts)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Post("/users", userHandler.Create)
+
 	http.ListenAndServe(":8000", r)
 }
